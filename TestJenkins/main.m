605353ba3a09724ba8572115aba112cbfeb39269
Wei Cao <wei.cao@wz-inc.com>
2018-04-08 16:46:36 +0800

    first commit

---
+++ TestJenkins/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestJenkins
+//
+//  Created by lawn.cao on 2018/4/8.
+//  Copyright © 2018年 lawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
